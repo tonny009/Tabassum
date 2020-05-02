8de939c646db51dfcfd92ca4c40aa1747d818289
tonny009 <tabassum.islam009@gmail.com>
2020-05-02 14:47:05 +0600

    logregMerged

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button registButton ;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registButton = (Button)findViewById(R.id.angry_btn);
+
+        registButton.setOnClickListener(this);
     }
+    @Override
+    public void onClick(View v)
+    {
+        if(v.getId()==R.id.angry_btn){
+            intent = new Intent(MainActivity.this,login.class);
+            startActivity(intent);
+        }
+
+    }
+
 }
